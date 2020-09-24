ebbd0efe724c4754f54f5b9514e5a98be1077699
Laura Docherty <laurahdocherty@gmail.com>
2020-09-24 08:39:45 -0500

    shebang line added

---
+++ functions.py
@@ -0,0 +1,36 @@
+# all functions for ASCII Graphics API for drawing rectangles and squares
+
+#!/usr/bin/env python
+
+print('Hello, World')
+
+def render_canvas():
+
+    row = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    grid = []
+
+    for i in range(11):
+        grid.append(row)
+
+    for i in range(len(grid)):
+        print(grid[i])
+
+
+
+
+# def add_shape():
+
+
+# def clear_all_shapes():
+
+
+
+# def create_rectangle():
+
+
+
+# def change_rectangle_fill():
+
+
+
+# def translate():
